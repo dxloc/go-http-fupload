51f7f8b272452f9bcbb1e5c64b3a65d2af0cad98
dxloc <dxl.loc@outlook.com>
2026-02-23 01:47:16 +0700

    add URL validator

--- router/router.go
+++ router/router.go
@@ -22,6 +22,28 @@ type Config struct {
 
 var config Config
 
+func UrlValidator(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		for _, c := range r.URL.Path {
+			if c >= 'a' && c <= 'z' {
+				continue
+			}
+			if c >= 'A' && c <= 'Z' {
+				continue
+			}
+			if c >= '0' && c <= '9' {
+				continue
+			}
+			if c == '/' || c == '-' || c == '_' || c == '.' {
+				continue
+			}
+			api.ThrowMessage(http.StatusBadRequest, "invalid URL")
+		}
+		next.ServeHTTP(w, r)
+	})
+
+}
+
 func ProxyPass(url string) string {
 	if len(url) < len(config.BaseUri) {
 		api.ThrowMessage(http.StatusNotFound, http.StatusText(http.StatusNotFound))
@@ -48,6 +70,7 @@ func NewRouter(cfg Config) *mux.Router {
 
 	for _, route := range routes {
 		handler = route.HandlerFunc
+		handler = UrlValidator(handler)
 		handler = api.PanicMiddleware(handler)
 		handler = Logger(route.Name)(handler)
 		router.
